43b913f6138bc94c2f159d09ed5f2f4a7869dff6
Sam Gould <sjg326@gmail.com>
2018-01-03 15:29:59 -0800

    used treeize middleware to hydrate data

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const knex = require('knex')(DATABASE);
 
 const app = express();
 
+const Treeize = require('treeize');
+const placesToEat = new Treeize();
+
 // app.get('/restaurants', (req, res) => {
 //   knex.first('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id', 'grade', 'date as inspectionDate', 'score')
 //     .select(knex.raw("CONCAT(address_building_number, ' ', address_street, ' ', address_zipcode) as address"))
@@ -19,37 +22,40 @@ const app = express();
 
 
 
-const hydrate = function (data) {
-  const hydrated = {};
-  data.forEach(row => {
-    if (!(row.id in hydrated)) {
-      hydrated[row.id] = {
-        name : row.name,
-        cuisine : row.cuisine,
-        borough : row.borough,
-        grades : [] 
-      };
-    }
-    hydrated[row.id].grades.push({
-      gradeId : row.gradeId,
-      grade : row.grade,
-      score : row.score
-    });
-  });
-  return hydrated;
+// const hydrate = function (data) {
+//   const hydrated = {};
+//   data.forEach(row => {
+//     if (!(row.id in hydrated)) {
+//       hydrated[row.id] = {
+//         name : row.name,
+//         cuisine : row.cuisine,
+//         borough : row.borough,
+//         grades : [] 
+//       };
+//     }
+//     hydrated[row.id].grades.push({
+//       gradeId : row.gradeId,
+//       grade : row.grade,
+//       score : row.score
+//     });
+//   });
+//   return hydrated;
+// };
+
+const tree = function(data) {
+  return placesToEat.grow(data).getData();
 };
 
 app.get('/restaurants', (req, res) => {
-  knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+  knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as grades:gradeId', 'grade as grades:grade', 'score as grades:score')
     .from('restaurants')
     .innerJoin('grades', 'grades.restaurant_id', 'restaurants.id')
     .where('restaurants.id', 1)
     .orderBy('date', 'desc')
     .limit(10)
-    .then(results => res.json(hydrate(results))
+    // .then(results => res.json(hydrate(results))
+    .then(results => res.json(tree(results))
     );
 });
-
-// ADD ANSWERS HERE
 
 app.listen(PORT);
