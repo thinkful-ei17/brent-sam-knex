33ae73981bc75b5e71141f236d1796bf958b113b
Sam Gould <sjg326@gmail.com>
2018-01-03 13:11:13 -0800

    updated server.js for knex hydrate

--- drills.js
+++ drills.js
@@ -6,16 +6,67 @@ const knex = require('knex')(DATABASE);
 // clear the console before each run
 process.stdout.write('\x1Bc');
 
+const people = [
+  {
+    id: 2,
+    name: 'John Doe',
+    age: 34,
+    petName: 'Rex',
+    petType: 'dog',
+  },
+  {
+    id: 2,
+    name: 'John Doe',
+    age: 34,
+    petName: 'Fido',
+    petType: 'dog',
+  },
+  {
+    id: 3,
+    name: 'Mary Jane',
+    age: 19,
+    petName: 'Mittens',
+    petType: 'kitten',
+  },
+  {
+    id: 3,
+    name: 'Mary Jane',
+    age: 19,
+    petName: 'Fluffy',
+    petType: 'cat'
+  }
+];
+
+const hydrated = {};
+
+people.forEach(row => {
+  if (!(row.id in hydrated)) {
+    hydrated[row.id] = {
+      id: row.id,
+      name: row.name,
+      age: row.age,
+      pets: []
+    };
+  }
+  hydrated[row.id].pets.push({
+    name: row.petName,
+    type: row.petType,
+  });
+});
+
+console.log(hydrated);
+console.log(hydrated[2].pets);
+
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+// knex('restaurants')
+//   .where('id', '11')
+//   .del()
+//   .debug(true)
+//   .then(results => console.log(results))
+//   .catch(err => console.log(err));
 
 
-// Destroy the connection pool
+// // Destroy the connection pool
 knex.destroy().then(() => {
   console.log('database connection closed');
 });
--- server.js
+++ server.js
@@ -7,11 +7,47 @@ const knex = require('knex')(DATABASE);
 
 const app = express();
 
+// app.get('/restaurants', (req, res) => {
+//   knex.first('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id', 'grade', 'date as inspectionDate', 'score')
+//     .select(knex.raw("CONCAT(address_building_number, ' ', address_street, ' ', address_zipcode) as address"))
+//     .from('restaurants')
+//     .innerJoin('grades', 'grades.restaurant_id', 'restaurants.id')
+//     .where('restaurants.id', 1)
+//     .orderBy('date', 'desc')
+//     .then(results => res.json(results));
+// });
+
+
+
+const hydrate = function (data) {
+  const hydrated = {};
+  data.forEach(row => {
+    if (!(row.id in hydrated)) {
+      hydrated[row.id] = {
+        name : row.name,
+        cuisine : row.cuisine,
+        borough : row.borough,
+        grades : [] 
+      };
+    }
+    hydrated[row.id].grades.push({
+      gradeId : row.gradeId,
+      grade : row.grade,
+      score : row.score
+    });
+  });
+  return hydrated;
+};
+
 app.get('/restaurants', (req, res) => {
-  knex.select('id', 'name', 'cuisine', 'borough')
+  knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
     .from('restaurants')
+    .innerJoin('grades', 'grades.restaurant_id', 'restaurants.id')
+    .where('restaurants.id', 1)
+    .orderBy('date', 'desc')
     .limit(10)
-    .then(results => res.json(results));
+    .then(results => res.json(hydrate(results))
+    );
 });
 
 // ADD ANSWERS HERE
